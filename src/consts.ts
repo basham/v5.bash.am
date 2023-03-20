dce4348f4c0adf5be4b760335280225c1f07263b
Chris Basham <chris@bash.am>
2023-03-20 19:59:47 -0400

    Make years of experience dynamic

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Chris Basham';
 export const SITE_DESCRIPTION = 'Chris Basham is a designer and front-end developer living in Bloomington, Indiana.';
+
+const currentYear = (new Date()).getFullYear();
+const firstYearOfExperience = 2011;
+export const YEARS_OF_EXPERIENCE = currentYear - firstYearOfExperience;
