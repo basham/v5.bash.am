5c6d3859ee3bda7593a08ae597af20f74b57f437
Chris Basham <chris@bash.am>
2023-03-23 22:23:04 -0400

    Clean schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,10 @@ const writings = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		pubDate: z
-			.date()
-			.transform((d) => new Date(d.getTime() + Math.abs(d.getTimezoneOffset() * 60000))),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+		pubDate: z.date().transform((d) =>
+			new Date(d.getTime() + Math.abs(d.getTimezoneOffset() * 60000))
+		)
+	})
 });
 
 export const collections = { writings };
