ef6739c83696bb637797924b8fc2ad5c0f698cdd
Chris Basham <chris@bash.am>
2024-02-21 11:31:45 -0500

    Refine resume and summary

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,9 @@
 export const SITE_TITLE = 'Chris Basham';
-export const SITE_DESCRIPTION = 'Chris Basham is a user experience designer and front-end developer in Bloomington, Indiana.';
+export const SITE_DESCRIPTION = 'Chris Basham is a user experience designer and front-end developer in southern Indiana.';
 
-const currentYear = (new Date()).getFullYear();
 const firstYearOfExperience = 2011;
-export const YEARS_OF_EXPERIENCE = currentYear - firstYearOfExperience;
+const firstMonthOfExperience = 6;
+const currentYear = (new Date()).getFullYear();
+const currentMonth = (new Date()).getMonth() + 1;
+const offsetYear = currentMonth - firstMonthOfExperience < 0 ? -1 : 0;
+export const YEARS_OF_EXPERIENCE = currentYear - firstYearOfExperience + offsetYear;
